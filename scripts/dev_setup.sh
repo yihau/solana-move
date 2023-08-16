365778f9735df91fae44cd5d936251979b47ed9d
yihau <yihau.chen@icloud.com>
2023-08-17 03:41:36 +0800

    commit 2

--- scripts/dev_setup.sh
+++ scripts/dev_setup.sh
@@ -24,6 +24,7 @@ SOLC_VERSION="v0.8.11+commit.d7f03943"
 SCRIPT_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
 cd "$SCRIPT_PATH/.." || exit
 
+
 function usage {
   echo "Usage:"
   echo "Installs or updates necessary dev tools for Move."
